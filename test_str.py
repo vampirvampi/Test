92ff91f55b5b446776b48ae5893963f5f45f4ddb
Roman <opter12345@gmail.com>
2021-03-03 19:57:43 +1000

    str test done

---
+++ test_str.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+def len_is_odd(str):
+    try:
+        return (len(str) % 2 == 0)
+    except TypeError:
+        return False
+
+
+class Teststr:
+    def test_1(self):
+        assert len_is_odd("") == True
+
+    def test_2(self):
+        assert len_is_odd("1111") == True
+
+    @pytest.mark.parametrize("x", ['', '1'])
+    def test_3(self,x):
+        assert len_is_odd(x) == False
